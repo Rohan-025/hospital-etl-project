4f311b480d30e143a71c5b3a69a42a4e7cf92237
Rohan-025 <sanwalrohan@gmail.com>
2025-08-01 17:45:28 +0530

    Added new metadata file and metadata table code in the file

---
+++ sql/dev/create_metadata_tables.sql
@@ -0,0 +1,18 @@
+
+
+-- creating the metadata TABLE
+
+create table dbo.ADF_Load_Metadata (
+    SourceName NVARCHAR(128),
+    FilePath          NVARCHAR(400),  -- ADLS path or wildcard, e.g., 'raw/hospital/patient/*.csv'
+    FileFormat        NVARCHAR(10),   -- 'csv' or 'parquet'
+    TargetSchema      NVARCHAR(64),   -- 'dbo'
+    TargetTable       NVARCHAR(128),  -- 'Patient'
+    StagingTable      NVARCHAR(128),  -- 'Patient_stg'
+    LoadType          NVARCHAR(20),   -- 'Full' or 'Incremental'
+    WatermarkColumn   NVARCHAR(64),   -- e.g., 'UpdatedAt'
+    BusinessKey       NVARCHAR(128),  -- e.g., 'PatientId'
+    -- add more columns as needed (column mapping, delimiter, etc.)
+    Active            BIT             -- enables/disables the mapping
+);
+
