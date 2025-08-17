4e6ffefec00b2d3faa1d7d3294c20b45fc7a8672
Rohan-025 <sanwalrohan@gmail.com>
2025-08-17 21:08:31 +0530

    Cloned new DB, made tables for both the DB hopital-A and hopital-B

--- sql/dev/create_files.sql
+++ sql/dev/create_files.sql
@@ -1,6 +1,8 @@
 
 drop table if exists dbo.patients
 
+-- Hospital-A Tables
+
 CREATE TABLE dbo.patients (
     patient_id VARCHAR(20) PRIMARY KEY,
     FirstName VARCHAR(50),
@@ -26,3 +28,159 @@ drop TABLE dbo.ADF_Load_Metadata
 
 drop TABLE dbo.patients
 
+
+-- creating departments for hospital-A
+
+create table dbo.departments(
+    DeptID NVARCHAR(50) not null,
+    Name nvarchar(50) not null,
+    constraint PK_departments PRIMARY KEY (DeptID)
+
+);
+
+
+-- encounters table hospital-A
+
+CREATE TABLE dbo.encounters (
+    EncounterID nvarchar(50) NOT NULL,
+    PatientID nvarchar(50) NOT NULL,
+    EncounterDate date NOT NULL,
+    EncounterType nvarchar(50) NOT NULL,
+    ProviderID nvarchar(50) NOT NULL,
+    DepartmentID nvarchar(50) NOT NULL,
+    ProcedureCode int NOT NULL,
+    InsertedDate date NOT NULL,
+    ModifiedDate date NOT NULL,
+    CONSTRAINT PK_enocunters PRIMARY KEY(EncounterID)
+);
+
+
+-- providers table hospital-A
+
+CREATE TABLE dbo.providers (
+    ProviderID nvarchar(50) NOT NULL,
+    FirstName nvarchar(50) NOT NULL,
+    LastName nvarchar(50) NOT NULL,
+    Specialization nvarchar(50) NOT NULL,
+    DeptID nvarchar(50) NOT NULL,
+    NPI bigint NOT NULL,
+    CONSTRAINT PK_provider PRIMARY KEY(ProviderID)
+);
+
+-- trancsactions table Hopital-A
+
+CREATE TABLE dbo.transactions (
+    TransactionID nvarchar(50) NOT NULL,
+    EncounterID nvarchar(50) NOT NULL,
+    PatientID nvarchar(50) NOT NULL,
+    ProviderID nvarchar(50) NOT NULL,
+    DeptID nvarchar(50) NOT NULL,
+    VisitDate date NOT NULL,
+    ServiceDate date NOT NULL,
+    PaidDate date NOT NULL,
+    VisitType nvarchar(50) NOT NULL,
+    Amount float NOT NULL,
+    AmountType nvarchar(50) NOT NULL,
+    PaidAmount float NOT NULL,
+    ClaimID nvarchar(50) NOT NULL,
+    PayorID nvarchar(50) NOT NULL,
+    ProcedureCode int NOT NULL,
+    ICDCode nvarchar(50) NOT NULL,
+    LineOfBusiness nvarchar(50) NOT NULL,
+    MedicaidID nvarchar(50) NOT NULL,
+    MedicareID nvarchar(50) NOT NULL,
+    InsertDate date NOT NULL,
+    ModifiedDate date NOT NULL,
+    CONSTRAINT PK_transactions PRIMARY KEY(TransactionID)
+);
+
+
+-- Creating tables for DB hopital-B
+
+
+-- droping an existing table, please double chekc the DB you are connected to always
+
+DROP table if EXISTS dbo.departments;
+
+-- departments table hopital-B
+
+CREATE TABLE dbo.departments (
+    DeptID nvarchar(50) NOT NULL,
+    Name nvarchar(50) NOT NULL,
+    CONSTRAINT PK_deparments PRIMARY KEY(DeptID)
+);
+
+
+-- encounters table hospital-B
+
+CREATE TABLE dbo.encounters (
+    EncounterID nvarchar(50) NOT NULL,
+    PatientID nvarchar(50) NOT NULL,
+    EncounterDate date NOT NULL,
+    EncounterType nvarchar(50) NOT NULL,
+    ProviderID nvarchar(50) NOT NULL,
+    DepartmentID nvarchar(50) NOT NULL,
+    ProcedureCode int NOT NULL,
+    InsertedDate date NOT NULL,
+    ModifiedDate date NOT NULL,
+    CONSTRAINT PK_encounters PRIMARY KEY (EncounterID)
+);
+
+
+-- patients table hospital-B
+
+CREATE TABLE dbo.patients (
+    ID nvarchar(50) NOT NULL,
+    F_Name nvarchar(50) NOT NULL,
+    L_Name nvarchar(50) NOT NULL,
+    M_Name nvarchar(50) NOT NULL,
+    SSN nvarchar(50) NOT NULL,
+    PhoneNumber nvarchar(50) NOT NULL,
+    Gender nvarchar(50) NOT NULL,
+    DOB date NOT NULL,
+    Address nvarchar(100) NOT NULL,
+    Updated_Date date NOT NULL,
+    CONSTRAINT PK_patients PRIMARY KEY (ID)
+);
+
+
+-- providers table hospital-B
+
+CREATE TABLE dbo.providers (
+    ProviderID nvarchar(50) NOT NULL,
+    FirstName nvarchar(50) NOT NULL,
+    LastName nvarchar(50) NOT NULL,
+    Specialization nvarchar(50) NOT NULL,
+    DeptID nvarchar(50) NOT NULL,
+    NPI bigint NOT NULL,
+    CONSTRAINT PK_providers PRIMARY KEY (ProviderID)
+);
+
+
+-- transanctions table hospital-B
+
+
+CREATE TABLE dbo.transactions (
+    TransactionID nvarchar(50) NOT NULL,
+    EncounterID nvarchar(50) NOT NULL,
+    PatientID nvarchar(50) NOT NULL,
+    ProviderID nvarchar(50) NOT NULL,
+    DeptID nvarchar(50) NOT NULL,
+    VisitDate date NOT NULL,
+    ServiceDate date NOT NULL,
+    PaidDate date NOT NULL,
+    VisitType nvarchar(50) NOT NULL,
+    Amount float NOT NULL,
+    AmountType nvarchar(50) NOT NULL,
+    PaidAmount float NOT NULL,
+    ClaimID nvarchar(50) NOT NULL,
+    PayorID nvarchar(50) NOT NULL,
+    ProcedureCode int NOT NULL,
+    ICDCode nvarchar(50) NOT NULL,
+    LineOfBusiness nvarchar(50) NOT NULL,
+    MedicaidID nvarchar(50) NOT NULL,
+    MedicareID nvarchar(50) NOT NULL,
+    InsertDate date NOT NULL,
+    ModifiedDate date NOT NULL,
+    CONSTRAINT PK_transactions PRIMARY KEY (TransactionID)
+);
